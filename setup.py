dc5f7cba03e389b4bc6139cb555cf708c2360f33
george <jo8937@gmail.com>
2017-12-21 01:55:40 +0900

    version up

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
  
 setup(name='migbq',
-      version='0.0.57',
+      version='0.0.58',
       url='https://github.com/jo8937/rdbms-to-bigquery-data-loader',
       license='MIT',
       author='jo8937',
